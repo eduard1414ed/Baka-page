742a60815646d5cbaa352c0e57a70b1432c706b2
eduard1414ed <eduard1414ed@gmail.com>
2026-08-05 15:13:54 +0200

    Сделать общий шаблон, главную страницу, дизайн-токены, тёмную тему и обработку картинок

---
+++ src/data/platforms.js
@@ -0,0 +1,10 @@
+export const platforms = [
+	{ label: 'YouTube', url: 'https://www.youtube.com/@bakapodcast' },
+	{ label: 'Telegram', url: 'https://t.me/podcastbaka' },
+	{ label: 'Apple Podcasts', url: 'https://podcasts.apple.com/podcast/id1577387113' },
+	{ label: 'Яндекс Музыка', url: 'https://music.yandex.ru/album/16989745' },
+	{ label: 'Spotify', url: 'https://open.spotify.com/show/23VyxCbBLw6hh8NcsWZy7N' },
+	{ label: 'VK', url: 'https://vk.ru/podcast.baka' },
+	{ label: 'Boosty', url: 'https://boosty.to/bakapodcast' },
+	{ label: 'Patreon', url: 'https://www.patreon.com/bakapodcast' },
+];
